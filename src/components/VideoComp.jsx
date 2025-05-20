3a50e122c0352aee51ff0c1f85aa3746f5aae292
17ishan <iishansinhaa@gmail.com>
2025-05-20 16:02:05 +0530

    second commit

--- src/components/VideoComp.jsx
+++ src/components/VideoComp.jsx
@@ -1,3 +1,68 @@
+// import React, { useEffect, useRef } from 'react'
+// import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+// import { useParams } from 'react-router-dom';
+
+
+// const VideoComp = () => {
+
+//     const { roomID } = useParams();
+//     console.log(roomID);
+//     const containerRef = useRef(null);
+
+//     useEffect(() => {
+//         let myMeeting = async (element) => {
+
+//             if (!roomID || !containerRef.current) return;
+
+
+//             // const appID = 2046966053;
+//             const appID = 266870978;
+//             const serverSecret = "67e31ca01317b34d8c5c15477500e0ae";
+//             const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "Anonymous");
+//             const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+//             zp.joinRoom({
+//                 container: element,
+//                 sharedLinks: [
+//                     {
+//                         name: 'Personal link',
+//                         url:
+//                             window.location.protocol + '//' +
+//                             window.location.host + window.location.pathname +
+//                             '?roomID=' +
+//                             roomID,
+//                     },
+//                 ],
+//                 scenario: {
+//                     mode: ZegoUIKitPrebuilt.GroupCall,
+//                 },
+//             });
+//         };
+//         myMeeting();
+
+//         return () => {
+//             if (containerRef.current) {
+//                 containerRef.current.innerHTML = "";
+//             }
+//         };
+//     }, [roomID]);
+
+//     return (
+//         <div
+//             className="myCallContainer"
+//             ref={containerRef}
+//             style={{ width: '100vw', height: '100vh' }}
+//         ></div>
+//     );
+
+
+// };
+
+
+
+// export default VideoComp
+
+
 import React, { useEffect, useRef } from 'react'
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import { useParams } from 'react-router-dom';
@@ -15,7 +80,6 @@ const VideoComp = () => {
             if (!roomID || !containerRef.current) return;
 
 
-            // const appID = 2046966053;
             const appID = 266870978;
             const serverSecret = "67e31ca01317b34d8c5c15477500e0ae";
             const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "Anonymous");
